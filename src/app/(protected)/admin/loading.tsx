4146ff457d4b9fc354f58ecb8943fe2616190469
tahminatanjidamiti <mity2027@gmail.com>
2026-01-08 02:58:07 +0600

    added admin host and user profile

---
+++ src/app/(protected)/admin/loading.tsx
@@ -0,0 +1,10 @@
+import Loading from '@/components/ui/Loading'
+
+
+export default function AdminLoadingPage() {
+  return (
+    <div className="py-24">
+    <Loading/>
+    </div>
+  )
+}
---
+++ src/app/(protected)/user/loading.tsx
@@ -0,0 +1,10 @@
+import Loading from '@/components/ui/Loading'
+
+
+export default function UserLoadingPage() {
+  return (
+    <div className="py-24">
+    <Loading/>
+    </div>
+  )
+}
